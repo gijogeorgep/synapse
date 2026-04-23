3ae541fcea4b696b6192c4ef6039600b018da54d
gijo <gijogeorgep02@gmail.com>
2026-04-23 14:41:09 +0530

    pwa

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,3 +11,16 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </AuthProvider>
   </React.StrictMode>,
 )
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then((registration) => {
+        console.log('SW registered: ', registration);
+      })
+      .catch((registrationError) => {
+        console.log('SW registration failed: ', registrationError);
+      });
+  });
+}
+
